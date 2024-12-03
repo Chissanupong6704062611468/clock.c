f9b2af9a319508f00fd28de82133fca00e155f86
6704062611468 <s6704062611468@email.kmutnb.ac.th>
2024-12-03 14:23:10 +0700

    Create clock.c

---
+++ clock.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int h = 10;
+	int m = 59;
+	int s = 30;
+	scanf("%d",&h);
+	scanf("%d",&m);
+	scanf("%d",&s);
+	
+	printf("hour:%d\nminute:%d\nsecond:%d",h,m,s);
+}
